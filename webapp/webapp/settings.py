82f18936ba6b56a4f18bed14c8a4622002c9ffb1
apeksha <apeksha05hegde@gmail.com>
2022-04-26 21:19:55 +0530

    final

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -126,5 +126,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #added manually 
-STATICFILES_DIR = [
+STATICFILES_DIR = [os.path.join(BASE_DIR, "static")
 ]
