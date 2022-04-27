9c7bf627fd68e19faa05e6ad5e872464acbf62f5
hrishik18 <hrishik.s@somaiya.edu>
2022-04-27 22:38:40 +0530

    hrishik final

--- webapp/Home/views.py
+++ webapp/Home/views.py
@@ -15,7 +15,7 @@ def home(request):
 
 
 def dataprocess(request):
-    name= request.POST.get('company', False)
+    name = request.POST.get('company', False)
     year = 2022-(int(request.POST.get('year', False)))
     transmission = request.POST.get('transmission', False)
     fuel = request.POST.get('fuel', False)
@@ -28,40 +28,39 @@ def dataprocess(request):
     tdistance = int(request.POST.get('tdistance', False))
 
     a = open(
-        r'C:\Users\Apeksha\Desktop\Used-car-price-prediction\test.sav', 'rb')
+        r'D:\Study\VScode\KJ_SEM4\MP\Used-car-price-prediction\test.sav', 'rb')
     loaded_model = pickle.load(a)
     cityinput = "Location_"+city
     petrolinput = "Fuel_Type_"+fuel
     transinput = "Transmission_"+transmission
-    nameinput= "Company_"+name
-    ownerinput= "Owner_Type_"+owner
-    dic = {'Year':year, 'Kilometers_Driven':tdistance, 'Seats':seats, 'Mileage(km/kg)':milage, 'Engine(CC)':engine,
-       'Power(bhp)':power, 'Location_Bangalore':0, 'Location_Chennai':0,
-       'Location_Coimbatore':0, 'Location_Delhi':0, 'Location_Hyderabad':0,
-       'Location_Jaipur':0, 'Location_Kochi':0, 'Location_Kolkata':0,
-       'Location_Mumbai':0, 'Location_Pune':0, 'Fuel_Type_Diesel':0, 'Fuel_Type_LPG':0,
-       'Fuel_Type_Petrol':0, 'Transmission_Manual':0, 'Company_Audi':0,
-       'Company_Bmw':0, 'Company_Bentley':0, 'Company_Chevrolet':0, 'Company_Datsun':0,
-       'Company_Fiat':0, 'Company_Force':0, 'Company_Ford':0, 'Company_Honda':0,
-       'Company_Hyundai':0, 'Company_Isuzu':0, 'Company_Jaguar':0, 'Company_Jeep':0,
-       'Company_Lamborghini':0, 'Company_Land':0, 'Company_Mahindra':0,
-       'Company_Maruti':0, 'Company_Mercedes-Benz':0, 'Company_Mini':0,
-       'Company_Mitsubishi':0, 'Company_Nissan':0, 'Company_Porsche':0,
-       'Company_Renault':0, 'Company_Skoda':0, 'Company_Tata':0, 'Company_Toyota':0,
-       'Company_Volkswagen':0, 'Company_Volvo':0, 'Owner_Type_First':0,
-       'Owner_Type_Fourth & Above':0, 'Owner_Type_Second':0, 'Owner_Type_Third':0
+    nameinput = "Company_"+name
+    ownerinput = "Owner_Type_"+owner
+    dic = {'Year': year, 'Kilometers_Driven': tdistance, 'Seats': seats, 'Mileage(km/kg)': milage, 'Engine(CC)': engine,
+           'Power(bhp)': power, 'Location_Bangalore': 0, 'Location_Chennai': 0,
+           'Location_Coimbatore': 0, 'Location_Delhi': 0, 'Location_Hyderabad': 0,
+           'Location_Jaipur': 0, 'Location_Kochi': 0, 'Location_Kolkata': 0,
+           'Location_Mumbai': 0, 'Location_Pune': 0, 'Fuel_Type_Diesel': 0, 'Fuel_Type_LPG': 0,
+           'Fuel_Type_Petrol': 0, 'Transmission_Manual': 0, 'Company_Audi': 0,
+           'Company_Bmw': 0, 'Company_Bentley': 0, 'Company_Chevrolet': 0, 'Company_Datsun': 0,
+           'Company_Fiat': 0, 'Company_Force': 0, 'Company_Ford': 0, 'Company_Honda': 0,
+           'Company_Hyundai': 0, 'Company_Isuzu': 0, 'Company_Jaguar': 0, 'Company_Jeep': 0,
+           'Company_Lamborghini': 0, 'Company_Land': 0, 'Company_Mahindra': 0,
+           'Company_Maruti': 0, 'Company_Mercedes-Benz': 0, 'Company_Mini': 0,
+           'Company_Mitsubishi': 0, 'Company_Nissan': 0, 'Company_Porsche': 0,
+           'Company_Renault': 0, 'Company_Skoda': 0, 'Company_Tata': 0, 'Company_Toyota': 0,
+           'Company_Volkswagen': 0, 'Company_Volvo': 0, 'Owner_Type_First': 0,
+           'Owner_Type_Fourth & Above': 0, 'Owner_Type_Second': 0, 'Owner_Type_Third': 0
            }
     dic[f"{cityinput}"] = 1
     dic[f"{petrolinput}"] = 1
-    dic[f"{nameinput}"]=1
-    dic[f"{ownerinput}"]=1
+    dic[f"{nameinput}"] = 1
+    dic[f"{ownerinput}"] = 1
     if transmission == "Manual":
         dic[f"{transinput}"] = 1
     userval = pd.DataFrame(dic, index=[0])
-    print (len(userval.columns))
     y_pred = loaded_model.predict(userval)
     data = {
-        "name":name,
+        "name": name,
         "year": year,
         "transmission": transmission,
         "fuel": fuel,
@@ -75,23 +74,3 @@ def dataprocess(request):
         "result": str(round(y_pred[0], 2))
     }
     return render(request, 'display.html', {'data': data})
-
-# Year
-# Enter the year
-# Kilometers Driven
-# Enter the kilometers driven
-# Owner type
-# Enter the owner type
-# Seats
-# Enter the no. of seats
-# Mileage(km/kg)
-# Enter the mileage
-# Engine(CC)
-# Enter the engine
-# Power(bhp)
-# Enter the power
-# Location
-# Select Option
-# Fuel-Type
-# Select Option
-# Transmission
